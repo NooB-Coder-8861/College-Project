eb5e371b1809a393326927e558a670a080c580d2
Sayantan Das <sayantan22052002@gmail.com>
2023-04-22 01:49:09 +0530

    v0.1.1

--- SmartContract.sol
+++ SmartContract.sol
@@ -1,6 +1,7 @@
 pragma solidity >=0.5.0<0.9.0;                      //setting a long range to avoid any syntactical error
 
-contract Vote{                                      //Smart Contract 
+
+contract Vote{                                      //Smart Contract
 
     struct Candidate{                               //Structure for candidates
     string name;
@@ -19,11 +20,11 @@ Candidate[] public Candidates;                      //Array of Candidates
 
     address public admin;                           //Election Commissioner
 
-    constructor( string[] memory _name){            
+    constructor( string[] memory _name) public{
         
         admin = msg.sender;
 
-        for(uint i=0; i< _name.length; i++){        //Loding the name of Candidate and initialising voteCount as 0
+        for(uint i = 0;i < _name.length; i++){        //Loding the name of Candidate and initialising voteCount as 0
             Candidates.push(Candidate({
                 name: _name[i],
                 voteCount: 0
